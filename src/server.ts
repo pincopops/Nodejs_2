fd5da512e7219d8a52a085d9511e7196a805e449
Elio Scaletta <elioscal@hotmail.it>
2022-09-15 08:21:37 +0200

    Aggiunta pacchetti express e configurazione  server http tramite express.

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,19 @@
-const name: string = "Elio";
-console.log(name);
+//importiamo la libreria express che serve per far funzionare il server
+import express from "express";
+import "express-async-errors";
+
+//creiamo una istanza di express assegnando l'esecuzione ad "app"
+const app = express();
+
+//chiamiamo il server con una chiamata di tipo GET e spediamo una risposta 
+app.get("/", (request, response) => {
+    response.send("Server's up!");
+});
+
+//diamo al nostro server le informazioni riguardo quale porta "ascoltare". Di conseguenza
+//capterà le varie modifiche al reload del server
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`)
+});
