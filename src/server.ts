ae2047b7341bbee7d33ed51e41f650f595896ef6
Elio Scaletta <elioscal@hotmail.it>
2022-09-15 09:21:34 +0200

    Configurazione dotenv e implementazione environment variable.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import app from "./app";
 
 //diamo al nostro server le informazioni riguardo quale porta "ascoltare". Di conseguenza
 //capterà le varie modifiche al reload del server
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
